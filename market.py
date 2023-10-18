2dfd6d65960e65db474c014a8700734b48e85dac
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-10-18 16:15:33 -0400

    template inheritance added

--- market.py
+++ market.py
@@ -3,7 +3,16 @@ from flask import render_template
 
 app=Flask(__name__)
 
-@app.route('/<name>')
-@app.route('/')
-def about(name=None):
-    return render_template("home.html", name=name)
+@app.route('/home')
+def home_page(name='Asad'):
+    return render_template("greet_user.html", name=name)
+
+@app.route('/market')
+def market_page(): 
+    items= [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    
+    return render_template('market.html', items=items)
